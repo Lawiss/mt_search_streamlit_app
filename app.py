4af77f8fde4d163958a00269bc688b00317cfda4
Luis Ventura <30115537+Lawiss@users.noreply.github.com>
2022-06-03 12:26:23 +0200

    Init

---
+++ app.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+
+import streamlit as st
+from sklearn.metrics.pairwise import cosine_similarity
+
+from utils import compute_embeddings, load_data, load_model
+
+st.set_page_config(
+    layout="wide",
+    page_title="Mission Transition - Test du document embedding pour la recherche",
+    page_icon="🔍",
+)
+DATA_PATH = Path("data/")
+MT_AIDES_FILEPATH = DATA_PATH / "MT_aides.json"
+
+aides_df = load_data(MT_AIDES_FILEPATH)
+model = load_model()
+aides_embeddings = compute_embeddings(aides_df["full_text"].tolist(), model)
+
+query = st.text_input("Recherche :")
+
+input_embedding = model.encode(query)
+similarities = cosine_similarity([input_embedding], aides_embeddings)
+aides_df["similarity"] = similarities[0]
+
+top_6_df = aides_df.sort_values("similarity", ascending=False).head(6)
+
+cols_up, cols_down = st.columns(3), st.columns(3)
+
+if query:
+    for i, (_, item) in enumerate(top_6_df.iterrows()):
+        if i < 3:
+            cols_up[i].subheader(item["name"])
+            cols_up[i].expander("Voir la description").markdown(
+                item["description"], unsafe_allow_html=True
+            )
+        else:
+            cols_down[i - 3].subheader(item["name"])
+            cols_down[i - 3].expander("Voir la description").markdown(
+                item["description"], unsafe_allow_html=True
+            )
+else:
+    st.info("Entrez un terme de recherche pour commencer.")
---
+++ utils.py
@@ -0,0 +1,49 @@
+from os import PathLike
+from typing import List, Union
+
+import numpy as np
+import pandas as pd
+import streamlit as st
+from bs4 import BeautifulSoup
+from sentence_transformers import SentenceTransformer
+
+
+def remove_html_tags(text: str) -> str:
+    """
+    Remove HTML tags using beautifulsoup.
+    """
+    soup = BeautifulSoup(text, "lxml")
+    return soup.text
+
+
+def normalize_text(text: str) -> str:
+    """
+    Remove newlines
+    """
+    text = remove_html_tags(text)
+    return text.replace("\n", "").strip()
+
+
+@st.cache(allow_output_mutation=True)
+def load_data(data_path: Union[str, PathLike]) -> pd.DataFrame:
+
+    aides_df = pd.read_json(data_path)
+    aides_df["full_text"] = (
+        aides_df["name"] + " " + aides_df["description"].apply(normalize_text)
+    )
+    return aides_df
+
+
+@st.cache(allow_output_mutation=True)
+def load_model(
+    model_name: str = "dangvantuan/sentence-camembert-large",
+) -> SentenceTransformer:
+    model = SentenceTransformer(model_name)
+    model.max_seq_length = 512
+    return model
+
+
+@st.cache
+def compute_embeddings(texts_list: List[str], model) -> np.ndarray:
+    aides_vectors = model.encode(texts_list)
+    return aides_vectors
